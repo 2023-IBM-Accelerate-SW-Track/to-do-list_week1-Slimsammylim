1ed992ffd1a3b51702e1bf00409baffc8f9a44a6
Slimsammylim <67528004+Slimsammylim@users.noreply.github.com>
2023-06-08 00:32:04 -0400

    Page not showing but everything filled

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import { Button, TextField } from "@mui/material";
 
 class AddTodo extends Component {
     // A local react state of the this component with a content property set to nothing.
     constructor() {
       super();
       this.state = {
-        content: "",
+        // Make an empty list
       };
     }
     // The handleChange function updates the react state with the new input value provided from the user.
@@ -37,7 +38,20 @@ class AddTodo extends Component {
         // an OnClick event.
         // 3. The value of the text field also should reflect the local state of this component.
         <div>
-          
+          <TextField
+            label="Add New Item"
+            variant="outlined"
+            //onChange={call handleChange here}
+            //value={set the content state here}
+          />
+          <Button
+            style={{ marginLeft: "10px" }}
+            //onClick={call handleSubmit here}
+            variant="contained"
+            color="primary"
+          >
+            Add
+          </Button>
         </div>
       );
     }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
+import "../pages/Home.css";
 import React, { Component } from "react";
+import AddTodo from "../component/AddTodo";
+import Todos from "../component/todos";
 
 class Home extends Component {
   // A default state of this component with an empty list of todos.
   constructor() {
     super();
     this.state = {
-      // create your empty list here call it todos.
+      todos: []
     };
   }
   // the addTodo function simply creates a new array that includes the user submitted todo item and then
@@ -27,7 +30,8 @@ class Home extends Component {
   render() {
     return (
       <div className="Home">
-        <p> Replace this</p>
+        <Todos todos={this.state} />
+        <AddTodo addTodo={this.addTodo()} />
         <h1>Todo's </h1>
       </div>
     );
